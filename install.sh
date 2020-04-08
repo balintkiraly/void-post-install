cf84d6ceaf4a70eef45f097840c1706a0867a659
balintkiraly <balint.kiraly@protonmail.com>
2020-04-08 20:11:03 +0200

    Save version (latest commit hash) to ~/.vopi

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ echo "Install git and clone the VOPI repository to start installation..."
 
 sudo xbps-install git
 git clone https://github.com/balintkiraly/vopi.git /tmp/vopi
-cd /tmp/vopi || exit
+latest_commit_hash=$(git rev-parse HEAD)
+echo "$latest_commit_hash" > "$HOME/.vopi" || exit
 
+cd /tmp/vopi || exit
 bash ./src/main.sh
+rm -rf /tmp/vopi
